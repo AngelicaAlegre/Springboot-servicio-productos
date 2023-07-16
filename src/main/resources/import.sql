87964aa0e1d3320ab5857a4e6ce985211413c3d2
AngelicaAlegre <angelicaalegre802@gmail.com>
2023-07-16 22:31:44 +0200

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO productos (nombre, precio, create_at) VALUES('Notebook', 10000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Mouse', 5000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Auriculares', 8000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Pad', 3000, NOW());
